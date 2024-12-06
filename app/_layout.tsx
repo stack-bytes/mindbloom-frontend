0d93ee1d15da4e6b61546f724eec8b3cc5c31a1a
David-Andrei Popa <contact@popadavid.ro>
2024-12-06 20:55:37 +0200

    Added calendar support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
-import '~/global.css';
+import "~/global.css";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
-import { SplashScreen, Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { Platform } from 'react-native';
-import { NAV_THEME } from '~/lib/constants';
-import { useColorScheme } from '~/lib/useColorScheme';
-import { PortalHost } from '@rn-primitives/portal';
-import { ThemeToggle } from '~/components/ThemeToggle';
-import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { SplashScreen, Stack, Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { Platform } from "react-native";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+import { PortalHost } from "@rn-primitives/portal";
+import { ThemeToggle } from "~/components/ThemeToggle";
+import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -24,7 +29,7 @@ const DARK_THEME: Theme = {
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before getting the color scheme.
 SplashScreen.preventAutoHideAsync();
@@ -35,17 +40,17 @@ export default function RootLayout() {
 
   React.useEffect(() => {
     (async () => {
-      const theme = await AsyncStorage.getItem('theme');
-      if (Platform.OS === 'web') {
+      const theme = await AsyncStorage.getItem("theme");
+      if (Platform.OS === "web") {
         // Adds the background color to the html element to prevent white background on overscroll.
-        document.documentElement.classList.add('bg-background');
+        document.documentElement.classList.add("bg-background");
       }
       if (!theme) {
-        AsyncStorage.setItem('theme', colorScheme);
+        AsyncStorage.setItem("theme", colorScheme);
         setIsColorSchemeLoaded(true);
         return;
       }
-      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      const colorTheme = theme === "dark" ? "dark" : "light";
       if (colorTheme !== colorScheme) {
         setColorScheme(colorTheme);
         setAndroidNavigationBar(colorTheme);
@@ -65,16 +70,24 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
-      <Stack>
-        <Stack.Screen
-          name='index'
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+      <Tabs>
+        <Tabs.Screen
+          name="schedule"
           options={{
-            title: 'Starter Base',
+            title: "schedule",
             headerRight: () => <ThemeToggle />,
           }}
         />
-      </Stack>
+
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Starter Base",
+            headerRight: () => <ThemeToggle />,
+          }}
+        />
+      </Tabs>
       <PortalHost />
     </ThemeProvider>
   );
